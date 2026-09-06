bb124f9914b37e58b17756c60a78608e61b8e262
RyanMampilly <182558803+RyanMampilly@users.noreply.github.com>
2026-09-06 17:45:04 -0400

    Constructor, generic object & switch for events

--- program.c
+++ program.c
@@ -13,6 +13,8 @@
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
+const int FONT_WIDTH = 8;
+const int FONT_HEIGHT = 8;
 char windowTitle[] = "My SDL Project";
 
 SDL_Window *window;
@@ -33,11 +35,11 @@ struct {
 
 
 
+
 typedef struct { // 56 bytes
     SDL_FRect box; // Bounding box // 16 bytes
     char string[32]; // Text displayed on button // 32 bytes 
     void (*func)(); // Generic function so that different buttons can do different things when pressed // 8 bytes
-    // char padding[8]; // In case I want to pad these objects to 64 bytes
 } Button;
 
 typedef struct { // 144 bytes
@@ -46,22 +48,71 @@ typedef struct { // 144 bytes
                       // For now, this will be the maximum amount of text a textbox CAN hold
 } TextBox;
 
+enum objectType {
+    BUTTON,
+    TEXT_BOX
+};
 
+typedef struct {
+    enum objectType type;
+    union {
+        Button button;
+        TextBox textBox;
+    };
+} Object;
+
+
+
+
+// Button constructor
+Object makeButton( float x, float y, float w, float h, char string[], void (*func)()) {
+    Object o; Button b;
+
+    int maximum = 32;
+    if (w / FONT_WIDTH < 32) maximum = w / FONT_WIDTH; 
+    if (strlen(string) > maximum) { 
+        strncpy(b.string, string, maximum - 4);
+        b.string[maximum - 4] = '\0';
+        strcat(b.string, "...");
+        b.string[maximum - 1] = '\0';
+    } else {
+        strcpy(b.string, string);
+    }
+    b.box = (SDL_FRect) {x, y, w, h}; b.func = func;
+    o.type = BUTTON; o.button = b;
+    return o;
+}
 
 // Takes in a SDL_FRect and two coordinates and checks if its in bounds of the rect
-bool inBounds(SDL_FRect *b, float x, float y) {
-    return (b->x < x) && (x < b->x + b->w) && (b->y < y) && (y < b->y + b->h);
+bool inBounds(SDL_FRect *box, float x, float y) {
+    return (box->x < x) && (x < box->x + box->w) && (box->y < y) && (y < box->y + box->h);
 }
 
 // Takes in a Button and handles rendering
-void renderButton(Button *b) {
-    if (mouseState.mouseDownButton == 1 && inBounds(&b->box, mouseState.mouseDownX, mouseState.mouseDownY)) SDL_SetRenderDrawColor(renderer, 200, 200, 200, SDL_ALPHA_OPAQUE);
-    else SDL_SetRenderDrawColor(renderer, 235, 235, 235, SDL_ALPHA_OPAQUE);
-    SDL_RenderFillRect(renderer, &b->box);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
-    SDL_RenderRect(renderer, &b->box);
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderDebugText(renderer, (b->box.x + b->box.w / 2 - strlen(b->string) * 4), (b->box.y + b->box.h / 2 - 4), b->string);
+void renderObject(Object *o) {
+    switch (o->type) {
+        case BUTTON:
+            if (mouseState.mouseDownButton == 1 && inBounds(&o->button.box, mouseState.mouseDownX, mouseState.mouseDownY)) 
+                SDL_SetRenderDrawColor(renderer, 200, 200, 200, SDL_ALPHA_OPAQUE);
+            else 
+                SDL_SetRenderDrawColor(renderer, 235, 235, 235, SDL_ALPHA_OPAQUE);
+
+            SDL_RenderFillRect(renderer, &o->button.box);
+            SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
+            SDL_RenderRect(renderer, &o->button.box);
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+            SDL_RenderDebugText(
+                    renderer, 
+                    (o->button.box.x + o->button.box.w / 2 - strlen(o->button.string) * (FONT_WIDTH / 2)), 
+                    (o->button.box.y + o->button.box.h / 2 - (FONT_HEIGHT / 2)),
+                    o->button.string
+            );
+        case TEXT_BOX:
+            break; //Write later
+        default:
+            printf("Invalid object sent for rendering\n");
+            break;
+    }
 }
 
 // Runs the object's function on click
@@ -101,14 +152,15 @@ int main(int argc, char **argv) {
         printf("Window created...\n");
     }
 
-    Button buttons[] = { //array of all the buttons in the program, and an enum so they can be accessed by name. hacky? maybe, i dont know
-        {{ 10, 10, 150, 50 }, "Button number 1", testFunction },
-        {{ 10, 70, 150, 50 }, "Button number 2", testFunction1 }
+    Object objects[] = { //array of all the objects in the program, and an enum so they can be accessed by name. hacky? maybe, i dont know
+        makeButton( 10, 10, 150, 50, "Button number 1", testFunction ),
+        makeButton( 10, 70, 150, 50, "abcdefghijklmnopqrstuvwxyzabcdefg", testFunction1 )
     }; 
-    enum buttonNames {
+    enum objectNames {
         test,
         otherTest
     };
+
 
     // Program loop
 
@@ -118,34 +170,37 @@ int main(int argc, char **argv) {
         // Prevent unnecessary rendering (may need to remove later) When there are no events, just repoll
         if (event.type == SDL_EVENT_POLL_SENTINEL) continue; 
 
-        printf("0x%x\n", event.type); //DEBUG
-        
-        //Maybe replace this if/else thing with a switch block? For SDL_EVENT_QUIT, should I use a goto (*gulp*)?
-        if (event.type == SDL_EVENT_QUIT) {
-            break;
-        } else if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) {
-            SDL_GetMouseState(&mouseState.mouseDownX, &mouseState.mouseDownY);
-            mouseState.mouseDownButton = event.button.button;
-        } else if (event.type == SDL_EVENT_MOUSE_BUTTON_UP) {
-            SDL_GetMouseState(&mouseState.mouseUpX, &mouseState.mouseUpY);
-            for (int i = 0; i < sizeof(buttons)/sizeof(buttons[0]); i++) {
-                onClick(&buttons[i]); //debug kinda
-            }
-            mouseState.mouseDownButton = 0;
-        } else if (event.type = SDL_EVENT_MOUSE_MOTION) {
-            SDL_GetMouseState(&mouseState.currentX, &mouseState.currentY);
+        switch (event.type) {
+            case SDL_EVENT_QUIT:
+                goto quit;
+            case SDL_EVENT_MOUSE_BUTTON_DOWN:
+                SDL_GetMouseState(&mouseState.mouseDownX, &mouseState.mouseDownY);
+                mouseState.mouseDownButton = event.button.button;
+                break;
+            case SDL_EVENT_MOUSE_BUTTON_UP:
+                SDL_GetMouseState(&mouseState.mouseUpX, &mouseState.mouseUpY);
+                for (int i = 0; i < sizeof(objects)/sizeof(objects[0]); i++) {
+                    onClick(&objects[i].button); //debug kinda
+                }
+                mouseState.mouseDownButton = 0;
+                break;
+            case SDL_EVENT_MOUSE_MOTION:
+                SDL_GetMouseState(&mouseState.currentX, &mouseState.currentY);
+                break;
         }
 
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
 
-        for (int i = 0; i < sizeof(buttons)/sizeof(buttons[0]); i++) {
-            renderButton(&buttons[i]);
+        for (int i = 0; i < sizeof(objects)/sizeof(objects[1]); i++) {
+            renderObject(&objects[i]);
         }
         SDL_RenderPresent(renderer);
     }
 
     // Quitting
+
+quit:
 
     SDL_Quit();
 
